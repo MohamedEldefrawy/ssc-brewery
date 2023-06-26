4473cdc020cd8183aff3ca87f6821b025bf8d497
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-06-26 16:04:31 +0300

    Create RestHeaderAuthFilter

--- src/main/java/guru/sfg/brewery/config/SecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfiguration.java
@@ -1,33 +1,45 @@
 package guru.sfg.brewery.config;
 
 import guru.sfg.brewery.web.security.CustomPasswordEncoder;
+import guru.sfg.brewery.web.security.RestHeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+        RestHeaderAuthFilter restHeaderAuthFilter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        restHeaderAuthFilter.setAuthenticationManager(authenticationManager);
+        return restHeaderAuthFilter;
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
 
         http
                 .authorizeRequests(expressionInterceptUrlRegistry ->
                         expressionInterceptUrlRegistry
                                 .antMatchers("/", "/webjars/**", "/resources/**").permitAll()
+                                .antMatchers( "/beers/find","/beers*").permitAll()
                                 .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                                .antMatchers(HttpMethod.GET, "/api/v1/beerUpc/**").permitAll())
+                                .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll())
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().and()
+                .csrf().disable()
                 .httpBasic();
+
     }
 
 
--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,15 +11,32 @@ class BeerRestControllerIT extends BaseIT {
 
     @Test
     @SneakyThrows
-    void testGetBeers()
-    {
-        mockMvc.perform(get("/api/v1/beer")).andExpect(status().isOk());
+    void testGetBeers() {
+        mockMvc.perform(get("/api/v1/beer").header("Api-Key", "admin").header("Api-Secret", "1234"))
+                .andExpect(status().isOk());
     }
 
     @Test
     @SneakyThrows
-    void testGetBeerById()
-    {
-        mockMvc.perform(get("/api/v1/beerUpc/0631234200036")).andExpect(status().isOk());
+    void testGetBeerByUpc() {
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200036")
+                        .header("Api-Key", "admin").header("Api-Secret", "1234"))
+                .andExpect(status().isOk());
     }
+
+    @Test
+    @SneakyThrows
+    void testGetBeerById() {
+        mockMvc.perform(get("/api/v1/beer/a3e60036-fe96-4768-a503-a7bb094bf9e1")
+                .header("Api-Key", "admin").header("Api-Secret", "1234")).andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    void testDeleteBeerById() {
+        mockMvc.perform(delete("/api/v1/beer/06aed155-336c-4e04-aca4-fd8da0e6c772")
+                        .header("Api-Key", "admin").header("Api-Secret", "1234"))
+                .andExpect(status().isOk());
+    }
+
 }
