4518a378ba86f5515f78da67839f40ef31a99a52
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-07-10 22:17:05 +0300

    Update JPA User

--- src/main/java/guru/sfg/brewery/domain/User.java
+++ src/main/java/guru/sfg/brewery/domain/User.java
@@ -1,6 +1,10 @@
 package guru.sfg.brewery.domain;
 
 import lombok.*;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -12,27 +16,13 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 @Builder
-public class User {
+public class User implements UserDetails, CredentialsContainer {
     @Id
     @GeneratedValue(strategy =
             GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @Singular
-    @JoinTable(name = "user_roles",
-            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
-            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
-    private Set<Role> roles;
-
-    @Transient
-    private Set<Authority> authorities;
-
-    public Set<Authority> getAuthorities() {
-        return this.roles.stream().map(Role::getAuthorities).flatMap(Set::stream).collect(Collectors.toSet());
-    }
-
     @Builder.Default
     private Boolean accountNonExpired = true;
     @Builder.Default
@@ -41,4 +31,43 @@ public class User {
     private Boolean credentialsNonExpired = true;
     @Builder.Default
     private Boolean enabled = true;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Singular
+    @JoinTable(name = "user_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private Set<Role> roles;
+
+    @Transient
+    private Set<GrantedAuthority> authorities;
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(Role::getAuthorities).flatMap(Set::stream).map(authority -> new SimpleGrantedAuthority(authority.getPermission())).collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.getAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.getAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.getCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.getEnabled();
+    }
+
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
+    }
 }
--- src/main/java/guru/sfg/brewery/services/UserDetailService.java
+++ src/main/java/guru/sfg/brewery/services/UserDetailService.java
@@ -1,38 +1,22 @@
 package guru.sfg.brewery.services;
 
-import guru.sfg.brewery.domain.Authority;
-import guru.sfg.brewery.domain.User;
 import guru.sfg.brewery.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserDetailService implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(), user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
-    }
-
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && !authorities.isEmpty()) {
-            return authorities.stream().map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else
-            return new HashSet<>();
+        log.info("Getting user info via JPA");
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
