39397e1676e4c35118bc182133da14f6897f083b
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-08-22 20:56:02 +0300

    add find all users by account locked status

--- src/main/java/guru/sfg/brewery/repositories/user/UserRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/user/UserRepository.java
@@ -1,10 +1,14 @@
-package guru.sfg.brewery.repositories;
+package guru.sfg.brewery.repositories.user;
 
 import guru.sfg.brewery.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByAccountNonLockedAAndLastModifiedDateIsBefore(Boolean locked, Timestamp timestamp);
 }
