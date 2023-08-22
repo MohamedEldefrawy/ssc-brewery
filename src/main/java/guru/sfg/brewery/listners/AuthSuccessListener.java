963648243bf8c35b4585818278f024981b42922d
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-08-22 18:09:14 +0300

    Get logged in user in authentication success listener

--- src/main/java/guru/sfg/brewery/listners/AuthSuccessListener.java
+++ src/main/java/guru/sfg/brewery/listners/AuthSuccessListener.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.listners;
 
+import guru.sfg.brewery.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +14,17 @@ public class AuthSuccessListener {
     @EventListener
     public void listen(AuthenticationSuccessEvent authenticationSuccessEvent) {
         log.debug("User logged in successfully");
+        if (authenticationSuccessEvent.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authenticationSuccessEvent.getSource();
+            if (token.getPrincipal() instanceof User) {
+                User user = (User) token.getPrincipal();
+                log.debug("Logged in username: " + user.getUsername());
+            }
+
+            if (token.getDetails() instanceof WebAuthenticationDetails) {
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+                log.debug("Logged in user address: " + details.getRemoteAddress());
+            }
+        }
     }
 }
