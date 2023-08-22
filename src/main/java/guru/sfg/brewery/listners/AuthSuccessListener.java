c6fcba5aeb07b1758dba05477e133a2fd082eed9
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-08-22 20:20:21 +0300

    Create LoginSuccess record after user login successfully

--- src/main/java/guru/sfg/brewery/listners/AuthSuccessListener.java
+++ src/main/java/guru/sfg/brewery/listners/AuthSuccessListener.java
@@ -1,6 +1,9 @@
 package guru.sfg.brewery.listners;
 
+import guru.sfg.brewery.domain.LoginSuccess;
 import guru.sfg.brewery.domain.User;
+import guru.sfg.brewery.repositories.LoginSuccessRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,16 +12,25 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AuthSuccessListener {
+    private final LoginSuccessRepository loginSuccessRepository;
+
     @EventListener
     public void listen(AuthenticationSuccessEvent authenticationSuccessEvent) {
         log.debug("User logged in successfully");
+
         if (authenticationSuccessEvent.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginSuccess.LoginSuccessBuilder loginSuccessBuilder = LoginSuccess.builder();
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authenticationSuccessEvent.getSource();
             if (token.getPrincipal() instanceof User) {
                 User user = (User) token.getPrincipal();
+                loginSuccessBuilder.user(user);
+                var loggedInUser = this.loginSuccessRepository.save(loginSuccessBuilder.build());
+
                 log.debug("Logged in username: " + user.getUsername());
+                log.debug("Logged in user id: " + loggedInUser.getId());
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
@@ -26,5 +38,6 @@ public class AuthSuccessListener {
                 log.debug("Logged in user address: " + details.getRemoteAddress());
             }
         }
+
     }
 }
