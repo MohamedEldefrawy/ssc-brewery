80a73266dd6b4c3ce8fe69f2b62ecd9110aa55f2
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-08-28 00:39:48 +0300

    Create Google2faFilter

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -64,4 +64,23 @@ public class UserController {
             return "user/register2fa";
         }
     }
+
+    @GetMapping("/verify2fa")
+    public String verify2fa() {
+        return "user/verify2fa";
+    }
+
+    @PostMapping
+    public String verifyPostOf2Fa(@RequestParam Integer verifyCode) {
+
+        User user = getUser();
+
+        if (googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setGoogle2FaRequired(false);
+
+            return "/index";
+        } else {
+            return "user/verify2fa";
+        }
+    }
 }
